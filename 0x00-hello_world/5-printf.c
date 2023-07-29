1de0f801fc2dc2df6ab5271c4224ebae00331703
Machinecode <machinecode@Ubuntu.myguest.virtualbox.org>
2023-07-29 04:58:37 -0700

    added new directory

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+/**
+ * main - a C program that prints text followed by a new line
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
