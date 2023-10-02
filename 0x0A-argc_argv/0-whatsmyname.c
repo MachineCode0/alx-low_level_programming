9b95d8e85455bdf73938fc5ee3adcdc5f1698b3d
Machinecode <machinecode@Ubuntu.myguest.virtualbox.org>
2023-10-02 05:25:55 -0700

    task 0 completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print the name of program
+ * @argc: argument count
+ * @argv: argument velocity
+ *
+ * Return: program name
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
