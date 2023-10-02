b7ecc00a53f071e737ddf994632dbc82ce879826
Machinecode <machinecode@Ubuntu.myguest.virtualbox.org>
2023-10-02 06:55:27 -0700

    task 2 completed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - a program that prints all arguments it receives.
+ * @argc: argument count
+ * @argv: argument name
+ *
+ * Return: all arguments received.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
