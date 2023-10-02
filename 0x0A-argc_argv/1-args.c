15c8325ba84fc8fc072bfc65437627fd549f2dfb
Machinecode <machinecode@Ubuntu.myguest.virtualbox.org>
2023-10-02 05:46:37 -0700

    task 1 completed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - a program that prints the number of arguments passed to it.
+ * @argc: argument count.
+ * @argv: argument name.
+ *
+ * Return: number of arguments passed.
+ */
+
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
